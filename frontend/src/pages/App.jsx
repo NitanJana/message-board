e143097e0cd319ec49f6d5a485b1aeac383c3507
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2024-02-26 08:33:21 +0530

    feat: add router

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import Footer from './components/Footer';
-import { baseURL } from './utils/constants';
+import Footer from '../components/Footer';
+import { baseURL } from '../utils/constants';
 
 const App = () => {
   const [messages, setMessages] = useState([]);
@@ -15,11 +15,11 @@ const App = () => {
   }, []);
 
   return (
-    <div className="bg-primary-blue font-roboto text-primary-cream grid min-h-screen grid-rows-[auto,1fr,auto] justify-items-center gap-10">
+    <div className="grid min-h-screen grid-rows-[auto,1fr,auto] justify-items-center gap-10 bg-primary-blue font-roboto text-primary-cream">
       <header className="m-4 text-3xl sm:text-7xl">Mini Message Board</header>
 
       <main className="grid h-full w-full grid-rows-[auto,1fr] items-center justify-items-center gap-10">
-        <button className="bg-primary-cream text-primary-blue hover:bg-primary-blue hover:text-primary-cream rounded-md border px-4 py-3 font-bold transition duration-300">
+        <button className="rounded-md border bg-primary-cream px-4 py-3 font-bold text-primary-blue transition duration-300 hover:bg-primary-blue hover:text-primary-cream">
           Add message
         </button>
 
---
+++ frontend/src/routers/routes.jsx
@@ -0,0 +1,11 @@
+import App from '../pages/App.jsx';
+
+const routes = [
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <ErrorPage />,
+  },
+];
+
+export default routes;
