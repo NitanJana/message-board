256ac7e8e8a8f6ac09e5a12aefca9329faa1e0c6
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2024-02-26 12:16:17 +0530

    feat: send current date in post req

--- frontend/src/pages/Form.jsx
+++ frontend/src/pages/Form.jsx
@@ -26,6 +26,7 @@ const Form = () => {
         {
           author,
           message,
+          sendDate: new Date(),
         },
         {
           headers: {
@@ -70,8 +71,17 @@ const Form = () => {
 
         <Button type="submit">
           {isSubmitting ? (
-            <div className="flex gap-2">
-              Submitting <img src="/submit.svg" alt="" className="animate-spin" />
+            <div className="group flex gap-2">
+              Submitting{' '}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24"
+                viewBox="0 -960 960 960"
+                width="24"
+                className="animate-spin fill-primary-blue group-hover:fill-primary-cream"
+              >
+                <path d="M204-318q-22-38-33-78t-11-82q0-134 93-228t227-94h7l-64-64 56-56 160 160-160 160-56-56 64-64h-7q-100 0-170 70.5T240-478q0 26 6 51t18 49l-60 60ZM481-40 321-200l160-160 56 56-64 64h7q100 0 170-70.5T720-482q0-26-6-51t-18-49l60-60q22 38 33 78t11 82q0 134-93 228t-227 94h-7l64 64-56 56Z" />
+              </svg>
             </div>
           ) : (
             'Submit'
